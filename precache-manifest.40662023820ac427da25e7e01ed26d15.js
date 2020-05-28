9e267c86645ad0735149c60bea890deb227f0761
RubenCameroAmador <rdcamero@uninorte.edu.co>
2020-05-27 20:43:27 -0500

    first commit

---
+++ precache-manifest.40662023820ac427da25e7e01ed26d15.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b22b832e5a6b1e669de22aab3c5d8b2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "45e5c07cf7f8f630e070",
+    "url": "/static/css/2.e080aa36.chunk.css"
+  },
+  {
+    "revision": "6ea7b7a5d64a5c6863b4",
+    "url": "/static/css/main.76386173.chunk.css"
+  },
+  {
+    "revision": "45e5c07cf7f8f630e070",
+    "url": "/static/js/2.60d168d2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.60d168d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ea7b7a5d64a5c6863b4",
+    "url": "/static/js/main.ac19bd83.chunk.js"
+  },
+  {
+    "revision": "bebcc9f39058dd97b5f1",
+    "url": "/static/js/runtime-main.78f559e5.js"
+  }
+]);
